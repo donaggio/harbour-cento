77f0ae4186dd141f35120e98b39f53052c976623
Luca Donaggio <donaggio@gmail.com>
2014-02-06 12:39:50 +0100

    v 0.5.1 Fix for high scores disappearing after OS upgrade to 1.0.3.8

--- src/harbour-cento.cpp
+++ src/harbour-cento.cpp
@@ -33,10 +33,16 @@ int main(int argc, char *argv[])
     // return SailfishApp::main(argc, argv);
 
     QScopedPointer<QGuiApplication> app(SailfishApp::application(argc, argv));
+    QScopedPointer<QQuickView> view(SailfishApp::createView());
     QTranslator translator;
+
     translator.load(QLocale::system().name(), SailfishApp::pathTo(QString("l10n")).toLocalFile());
     app->installTranslator(&translator);
-    QScopedPointer<QQuickView> view(SailfishApp::createView());
+
+    app->setOrganizationName("");
+    app->setOrganizationDomain("");
+    app->setApplicationName("harbour-cento");
+
     view->rootContext()->setContextProperty(QString("version"), QVariant(VERSION));
     view->setSource(SailfishApp::pathTo(QString("qml/harbour-cento.qml")));
     view->show();
